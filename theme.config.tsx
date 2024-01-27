9ac3775f4f877fb65f7e82b51c498b40f843d4d4
avenbreaks <dev-cosmware@gitshock.com>
2024-01-27 09:34:53 +0700

    add first page

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Cybria Documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Cybria-Chain/cybria-documentation',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://t.me/cybriacoin',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Cybria-Chain/cybria-documentation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Cybria Technical Documentation',
   },
 }
 
